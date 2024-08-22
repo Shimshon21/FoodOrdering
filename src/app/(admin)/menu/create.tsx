87c53701d1d77bccfdf1c0190a0972616ba73e77
Shimshon21 <shimpolak@gmail.com>
2024-08-22 18:59:53 +0300

    Added update option

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -3,82 +3,152 @@ import { defaultPizzaImage } from "@/components/ProductListItem";
 import Colors from "@/constants/Colors";
 import { useState } from "react";
 import * as ImagePicker from "expo-image-picker";
+import { Stack, useLocalSearchParams } from "expo-router";
+import Button from "@/components/Button";
 
 const CreateProductsScreen = () => {
-	const [name, setName] = useState("");
-	const [price, setPrice] = useState("");
-	const [errors, setErrors] = useState("");
-	const [image, setImage] = useState<string | null>(null);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [errors, setErrors] = useState("");
+  const [image, setImage] = useState<string | null>(null);
 
-	const pickImage = async () => {
-		// No permissions request is necessary for launching the image library
-		let result = await ImagePicker.launchImageLibraryAsync({
-			mediaTypes: ImagePicker.MediaTypeOptions.Images,
-			allowsEditing: true,
-			aspect: [4, 3],
-			quality: 0.5,
-		});
+  const { id } = useLocalSearchParams();
+  const isUpdating = !!id;
 
-		console.log(result);
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.5,
+    });
 
-		if (!result.canceled) {
-			setImage(result.assets[0].uri);
-		}
-	};
+    console.log(result);
 
-	return (
-		<View style={styles.container}>
-			<Image
-				source={{ uri: image || defaultPizzaImage }}
-				style={styles.image}
-				resizeMode="contain"
-			/>
-			<Text onPress={pickImage} style={styles.textButton}>
-				Select Image
-			</Text>
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
-			<Text style={styles.label}>Name</Text>
-			<TextInput placeholder="Name" style={styles.input} />
+  const resetFields = () => {
+    setName("");
+    setPrice("");
+  };
 
-			<Text style={styles.label}>Price</Text>
-			<TextInput
-				placeholder="Price"
-				style={styles.input}
-				keyboardType="decimal-pad"
-			/>
-		</View>
-	);
+  const validateInput = () => {
+    if (!name) {
+      setErrors("Name is required");
+      return false;
+    }
+
+    if (!price) {
+      setErrors("Price is required");
+      return false;
+    }
+
+    if (isNaN(parseFloat(price))) {
+      setErrors("Price is not a number");
+      return false;
+    }
+
+    return true;
+  };
+
+  const onSubmit = () => {
+    if (isUpdating) {
+      onUpdateCreate();
+    } else {
+      onCreate();
+    }
+  };
+
+  const onUpdateCreate = () => {
+    if (!validateInput()) {
+      return;
+    }
+
+    console.warn("Updating product: ");
+  };
+
+  const onCreate = () => {
+    if (!validateInput()) {
+      return;
+    }
+
+    console.warn("Creating product", name, price);
+    resetFields();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: isUpdating ? `Update Product ${id}` : "Create Product",
+        }}
+      />
+
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text onPress={pickImage} style={styles.textButton}>
+        Select Image
+      </Text>
+
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        placeholder="Name"
+        style={styles.input}
+        onChangeText={setName}
+      />
+
+      <Text style={styles.label}>Price($)</Text>
+      <TextInput
+        value={price}
+        placeholder="9.99"
+        style={styles.input}
+        keyboardType="decimal-pad"
+        onChangeText={setPrice}
+      />
+
+      <Text style={{ color: "red" }}>{errors}</Text>
+      <Button onPress={onCreate} text="Create" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		padding: 10,
-	},
-	image: {
-		aspectRatio: 1,
-		width: "50%",
-		alignSelf: "center",
-	},
-	textButton: {
-		alignSelf: "center",
-		fontWeight: "bold",
-		color: Colors.light.tint,
-		marginVertical: 10,
-	},
-	input: {
-		backgroundColor: "white",
-		padding: 10,
-		borderRadius: 5,
-		marginTop: 5,
-		marginBottom: 20,
-	},
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+  },
+  image: {
+    aspectRatio: 1,
+    width: "50%",
+    alignSelf: "center",
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 20,
+  },
 
-	label: {
-		color: "gray",
-		fontSize: 16,
-	},
+  label: {
+    color: "gray",
+    fontSize: 16,
+  },
 });
 
 export default CreateProductsScreen;
