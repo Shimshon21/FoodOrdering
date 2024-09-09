ce8322d3cee1a87a4f74b5983c5624cc4f0c3b0b
Shimshon polak <shimpolak@gmail.com>
2024-09-09 23:57:02 +0300

    Update index.tsx

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,17 +1,27 @@
 import products from "@assets/data/products";
 import ProductListItem from "@/components/ProductListItem";
 import { View, FlatList } from "react-native";
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
 
-export default function TabOneScreen() {
-  return (
-    <View>
-      <FlatList
-        renderItem={({ item }) => <ProductListItem product={item} />}
-        data={products}
-        numColumns={2}
-        contentContainerStyle={{ gap: 10, paddingHorizontal: 10 }} // gap between rows, padding of the container
-        columnWrapperStyle={{ gap: 10 }}
-      ></FlatList>
-    </View>
-  );
+export default function MenuScreen() {
+	// Calle when the component mounted(displaye)
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data, error } = await supabase.from("products").select("*");
+			console.log(error);
+		};
+	}, []);
+
+	return (
+		<View>
+			<FlatList
+				renderItem={({ item }) => <ProductListItem product={item} />}
+				data={products}
+				numColumns={2}
+				contentContainerStyle={{ gap: 10, paddingHorizontal: 10 }} // gap between rows, padding of the container
+				columnWrapperStyle={{ gap: 10 }}
+			></FlatList>
+		</View>
+	);
 }
