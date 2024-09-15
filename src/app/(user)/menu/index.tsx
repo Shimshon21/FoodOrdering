564d783040d8b4f147b7ed493efa8c04d19ecf8a
Shimshon21 <shimpolak@gmail.com>
2024-09-15 15:31:52 +0300

    Added react-query

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,27 +1,46 @@
 import products from "@assets/data/products";
 import ProductListItem from "@/components/ProductListItem";
-import { View, FlatList } from "react-native";
+import { View, FlatList, ActivityIndicator, Text } from "react-native";
 import { useEffect } from "react";
 import { supabase } from "@/lib/supabase";
+import { useQuery } from "@tanstack/react-query";
 
 export default function MenuScreen() {
-	// Calle when the component mounted(displaye)
-	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data, error } = await supabase.from("products").select("*");
-			console.log(error);
-		};
-	}, []);
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const { data, error } = await supabase.from("products").select("*");
+      if (error) {
+        throw new Error(error.message);
+      }
+      return data;
+    },
+  });
 
-	return (
-		<View>
-			<FlatList
-				renderItem={({ item }) => <ProductListItem product={item} />}
-				data={products}
-				numColumns={2}
-				contentContainerStyle={{ gap: 10, paddingHorizontal: 10 }} // gap between rows, padding of the container
-				columnWrapperStyle={{ gap: 10 }}
-			></FlatList>
-		</View>
-	);
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch products</Text>;
+  }
+  // // Calle when the component mounted(displaye)
+  // useEffect(() => {
+  // 	const fetchProducts = async () => {
+  // 		const { data, error } = await supabase.from("products").select("*");
+  // 		console.log(error);
+  // 	};
+  // }, []);
+
+  return (
+    <View>
+      <FlatList
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        data={data}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, paddingHorizontal: 10 }} // gap between rows, padding of the container
+        columnWrapperStyle={{ gap: 10 }}
+      ></FlatList>
+    </View>
+  );
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -4,58 +4,58 @@ import { Colors } from "react-native/Libraries/NewAppScreen";
 import { Link, useSegments } from "expo-router";
 
 type ProductListItemProps = {
-	product: Product;
+  product: Product;
 };
 
 export const defaultPizzaImage: string =
-	"https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
-	const segments = useSegments();
+  const segments = useSegments();
 
-	return (
-		// We can also write the route as (tabs)/menu/${product.id}
-		// but round brackets() are optional path
-		<Link href={`${segments[0]}/menu/${product.id}`} asChild={true}>
-			<Pressable style={styles.container}>
-				<Image
-					style={styles.image}
-					source={{ uri: product.image || defaultPizzaImage }}
-					resizeMode="contain"
-				></Image>
-				<Text style={styles.title}>{product.name}</Text>
-				<Text style={styles.price}>${product.price}</Text>
-			</Pressable>
-		</Link>
-	);
+  return (
+    // We can also write the route as (tabs)/menu/${product.id}
+    // but round brackets() are optional path
+    <Link href={`${segments[0]}/menu/${product.id}`} asChild={true}>
+      <Pressable style={styles.container}>
+        <Image
+          style={styles.image}
+          source={{ uri: product.image || defaultPizzaImage }}
+          resizeMode="contain"
+        ></Image>
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
+  );
 };
 
 export default ProductListItem;
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: "white",
-		padding: 10,
-		borderRadius: 20,
-		flex: 1,
-		maxWidth: "50%",
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: "80%",
-	},
-	image: {
-		width: "100%",
-		aspectRatio: 1,
-	},
-	title: {
-		fontSize: 18,
-		fontWeight: "600",
-		marginVertical: 10,
-	},
-	price: {
-		color: Colors.light.tint,
-		fontWeight: "bold",
-	},
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
 });
